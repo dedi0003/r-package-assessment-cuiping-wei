3027e3857d56c3ea30d938d5f3137cf6b3ebd864
cuiping wei <cwei0018@student.monash.edu>
2020-10-21 21:31:01 +1100

    Added population data due to possible API connection issue in future

---
+++ R/pop_data.R
@@ -0,0 +1,16 @@
+#' 2019 Population data
+#'
+#' A dataset containing 2019 population data for 221 countries.
+#'
+#' @format a tibble with 221 observations and 2 variables:
+#'
+#' - **country**: Country name
+#' - **population**: Population data for 2019
+#'
+#'
+#' @examples
+#' pop_data
+#'
+#'
+#'@source [World Bank](https://databank.worldbank.org/home.aspx)
+"pop_data"
--- inst/app/app/dataTidy.R
+++ inst/app/app/dataTidy.R
@@ -1,4 +1,5 @@
 # load libraries
+library(covid19)
 library(coronavirus)
 library(dplyr)
 library(wbstats)
@@ -21,42 +22,6 @@ data("coronavirus")
 
 ## Update the latest data
 update_dataset(silence = FALSE)
-
-# population data
-pop_data <- wb_data("SP.POP.TOTL", start_date = 2019, end_date = 2019) %>%
-  select(country, population = "SP.POP.TOTL") %>%
-  mutate(country = recode(country,
-                          "Bahamas, The" = "Bahamas",
-                          "Gambia, The" = "Gambia",
-                          "Kyrgyz Republic" = "Kyrgyzstan",
-                          "Lao PDR" = "Laos",
-                          "St. Kitts and Nevis" = "Saint Kitts and Nevis",
-                          "Syrian Arab Republic" = "Syria",
-                          "Brunei Darussalam" = "Brunei",
-                          "Congo, Dem. Rep." = "Congo (Kinshasa)",
-                          "Congo, Rep." = "Congo (Brazzaville)",
-                          "Czech Republic" = "Czechia",
-                          "Egypt, Arab Rep." = "Egypt",
-                          "Iran, Islamic Rep." = "Iran",
-                          "Korea, Rep." =  "Korea, South",
-                          "St. Lucia" = "Saint Lucia",
-                          "Russian Federation" = "Russia",
-                          "Slovak Republic" = "Slovakia",
-                          "United States" = "US",
-                          "St. Vincent and the Grenadines" = "Saint Vincent and the Grenadines",
-                          "Venezuela, RB" = "Venezuela",
-                          "Yemen, Rep." = "Yemen",
-                          "Myanmar" = "Burma"))
-
-
-## Data from worldometer, add data for no population countries
-add_pop <- data.frame(
-  country    = c("Holy See", "Western Sahara", "Taiwan", "MS Zaandam"),
-  population = c(801, 601115, 23827505, 76804)
-)
-
-## conbine together
-pop_data<- bind_rows(pop_data, add_pop)
 
 
 # Map plot section
